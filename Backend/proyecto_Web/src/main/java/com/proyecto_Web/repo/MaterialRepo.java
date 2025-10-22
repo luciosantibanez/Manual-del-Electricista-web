f36221a0e99a5c2be28ddd3fb43a948639b8a076
Lucio Santibañez <luciosantibanez99@gmail.com>
2025-10-22 20:00:08 -0300

    terminando entidad MaterialRepo.java para que pueda correr el programa

--- Backend/proyecto_Web/src/main/java/com/proyecto_Web/repo/MaterialRepo.java
+++ Backend/proyecto_Web/src/main/java/com/proyecto_Web/repo/MaterialRepo.java
@@ -1,18 +1,12 @@
 package com.proyecto_Web.repo;
 
 import com.proyecto_Web.domain.Material;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
-public class MaterialRepo extends JpaRepository<Material,Integer> {
+public interface MaterialRepo extends JpaRepository<Material, Integer> {
 
     @Query("""
         SELECT m FROM Material m
